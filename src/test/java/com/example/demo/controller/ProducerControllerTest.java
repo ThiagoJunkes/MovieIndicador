4530bdd0814bf5c2905c724385a41cac7c404c8a
ThiagoJunkes <thiago.junkes3@gmail.com>
2025-04-27 20:21:06 -0300

    logica result-test

--- src/test/java/com/example/demo/controller/ProducerControllerTest.java
+++ src/test/java/com/example/demo/controller/ProducerControllerTest.java
@@ -1,23 +1,26 @@
 package com.example.demo.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.example.demo.dto.AwardIntervalResponseDTO;
-import com.example.demo.dto.ProducerIntervalDTO;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ProducerControllerTest {
+class ProducerControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -26,25 +29,24 @@ public class ProducerControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
-    void testProducerIntervalResponse() throws Exception {
-        String json = mockMvc.perform(get("/producers/intervals"))
+    void testProducerIntervalResponse_ShouldMatchExpectedData() throws Exception {
+        // Chama API
+        String actualJson = mockMvc.perform(get("/producers/intervals"))
                 .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
 
-        AwardIntervalResponseDTO response = objectMapper.readValue(json, AwardIntervalResponseDTO.class);
+        InputStream is = getClass().getResourceAsStream("/expected/expected-test-result.json");
+        String expectedJson = new String(is.readAllBytes(), StandardCharsets.UTF_8);
 
-        List<ProducerIntervalDTO> minList = response.getMin();
-        List<ProducerIntervalDTO> maxList = response.getMax();
+        AwardIntervalResponseDTO actualResponse = objectMapper.readValue(actualJson, new TypeReference<>() {});
+        AwardIntervalResponseDTO expectedResponse = objectMapper.readValue(expectedJson, new TypeReference<>() {});
 
-        assertThat(minList).isNotEmpty();
-        assertThat(maxList).isNotEmpty();
-
-        for (ProducerIntervalDTO dto : minList) {
-            assertThat(dto.getInterval()).isGreaterThanOrEqualTo(0);
-        }
-
-        for (ProducerIntervalDTO dto : maxList) {
-            assertThat(dto.getInterval()).isGreaterThanOrEqualTo(0);
-        }
+        // Compara se objeto está EXATAMENTE igual ao esperado
+        assertThat(actualResponse)
+                .usingRecursiveComparison()
+                .ignoringCollectionOrder()
+                .isEqualTo(expectedResponse);
     }
 }
